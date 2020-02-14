6a692403177f7c8be4afd511b38f9268c8917040
Yufeng-L <yflin@bu.edu>
2020-02-14 01:31:22 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Course{
+  constructor(college,department,number,section,professor,key) {
+    this.college = college;
+    this.department = department;
+    this.number = number;
+    this.section = section;
+    this.professor = professor;
+    this.key = key;
+  }
 }
 
+class App extends Component {
+  render() {
+    let courses = [
+      new Course('CAS', 'CS','112','B1','Sullivan',0),
+      new Course('CAS', 'CS','332','B1','Bun',1),
+      new Course('CAS', 'CS','350','B1','Sarkar',2),
+      new Course('ENG','EC','602','A2','Carruthers',3),
+      new Course('CAS', 'CS','542','A1','Sarah',4),
+      ];
+    return(
+        <div className = "App">
+          {courses.map(function(item) {
+            return (
+                <div identifier = {item.key}>
+                <table>
+                  <thead>
+                  <tr>
+                    <th>College</th>
+                    <th>Department</th>
+                    <th>Number</th>
+                    <th>Section</th>
+                    <th>Professor</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                  <td>{item.college}</td>
+                  <td>{item.department}</td>
+                  <td>{item.number}</td>
+                  <td>{item.section}</td>
+                  <td>{item.professor}</td>
+                  </tr>
+                  </tbody>
+                </table>
+                </div>
+
+            );
+          })}
+
+        </div>
+    )
+
+  }
+
+}
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+//how module replacement, good to use if to debug
+if (module.hot) {
+    module.hot.accept();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
